59f40b0b3de31941be75736ee997460571efdd33
ido <idok@wix.com>
2015-02-04 16:30:26 +0200

    rt html ext

---
+++ src/com/wix/rt/codeInsight/RTHtmlExtension.java
@@ -0,0 +1,78 @@
+package com.wix.rt.codeInsight;
+
+import com.intellij.psi.PsiElement;
+import com.intellij.psi.PsiFile;
+import com.intellij.psi.html.HtmlTag;
+import com.intellij.psi.impl.source.xml.SchemaPrefix;
+import com.intellij.psi.util.PsiElementFilter;
+import com.intellij.psi.util.PsiTreeUtil;
+import com.intellij.psi.xml.XmlFile;
+import com.intellij.psi.xml.XmlTag;
+import com.intellij.xml.HtmlXmlExtension;
+import com.wix.rt.actions.RTActionUtil;
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class RTHtmlExtension extends HtmlXmlExtension {
+    @Override
+    public boolean isAvailable(PsiFile file) {
+        return super.isAvailable(file) && RTActionUtil.isRTEnabled(file.getProject());
+    }
+
+    @Override
+    public boolean isRequiredAttributeImplicitlyPresent(XmlTag tag, String attrName) {
+//        for (XmlAttribute attribute : tag.getAttributes()) {
+//            if (("rt-" + attrName).equals(DirectiveUtil.normalizeAttributeName(attribute.getName()))) {
+//                return true;
+//            }
+//        }
+        return super.isRequiredAttributeImplicitlyPresent(tag, attrName);
+    }
+
+    @Override
+    public SchemaPrefix getPrefixDeclaration(XmlTag context, String namespacePrefix) {
+//        if ("rt".equals(namespacePrefix)) {
+//            return new SchemaPrefix(null, null, namespacePrefix);
+//        }
+        return super.getPrefixDeclaration(context, namespacePrefix);
+    }
+
+    @NotNull
+    @Override
+    public List<TagInfo> getAvailableTagNames(@NotNull XmlFile file, @NotNull XmlTag context) {
+        List<TagInfo> list = super.getAvailableTagNames(file, context);
+        list.add(new TagInfo(RTTagDescriptorsProvider.RT_REQUIRE, "http://www.w3.org/1999/html"));
+        List<String> importedTags = loadImportedTags(file, context);
+        for (String importedTag : importedTags) {
+            list.add(new TagInfo(importedTag, "http://www.w3.org/1999/html") {
+                @Nullable
+                @Override
+                public PsiElement getDeclaration() {
+                    return super.getDeclaration();
+                }
+            });
+        }
+        return list;
+    }
+
+    private static List<String> loadImportedTags(@NotNull XmlFile file, @NotNull XmlTag context) {
+//        PsiElement[] arr = file.getRootTag().getChildren();
+//        Collection<HtmlTag> tags = PsiTreeUtil.findChildrenOfType(file, HtmlTag.class);
+        PsiElement[] reqTags = PsiTreeUtil.collectElements(file, new PsiElementFilter() {
+            @Override
+            public boolean isAccepted(PsiElement element) {
+                return element instanceof HtmlTag && ((HtmlTag) element).getName().equals(RTTagDescriptorsProvider.RT_REQUIRE);
+            }
+        });
+
+        List<String> importedTags = new ArrayList<String>();
+        for (PsiElement elem : reqTags) {
+            String as = ((HtmlTag) elem).getAttributeValue("as");
+            importedTags.add(as);
+        }
+        return importedTags;
+    }
+}
---
+++ src/com/wix/rt/codeInsight/RTTagDescriptorsProvider.java
@@ -0,0 +1,57 @@
+package com.wix.rt.codeInsight;
+
+import com.intellij.codeInsight.completion.XmlTagInsertHandler;
+import com.intellij.codeInsight.lookup.LookupElement;
+import com.intellij.codeInsight.lookup.LookupElementBuilder;
+import com.intellij.lang.javascript.index.JSNamedElementProxy;
+import com.intellij.psi.html.HtmlTag;
+import com.intellij.psi.impl.source.xml.XmlElementDescriptorProvider;
+import com.intellij.psi.xml.XmlTag;
+import com.intellij.xml.XmlElementDescriptor;
+import com.intellij.xml.XmlTagNameProvider;
+import com.wix.rt.RTProjectComponent;
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
+
+import java.util.List;
+
+public class RTTagDescriptorsProvider implements XmlElementDescriptorProvider, XmlTagNameProvider {
+
+    public static final String RT_REQUIRE = "rt-require";
+    public static final String AS = "as";
+    public static final String DEPENDENCY = "dependency";
+
+    @Override
+    public void addTagNameVariants(final List<LookupElement> elements, @NotNull XmlTag xmlTag, String prefix) {
+//        System.out.println("");
+//        if (!(xmlTag instanceof HtmlTag && AngularIndexUtil.hasAngularJS(xmlTag.getProject()))) return;
+//
+//        final Project project = xmlTag.getProject();
+//        DirectiveUtil.processTagDirectives(project, new Processor<JSNamedElementProxy>() {
+//            @Override
+//            public boolean process(JSNamedElementProxy directive) {
+//                addLookupItem(elements, directive);
+//                return true;
+//            }
+//        });
+    }
+
+    private static void addLookupItem(List<LookupElement> elements, JSNamedElementProxy directive) {
+        elements.add(LookupElementBuilder.create(directive).withInsertHandler(XmlTagInsertHandler.INSTANCE));
+    }
+
+    @Nullable
+    @Override
+    public XmlElementDescriptor getDescriptor(XmlTag xmlTag) {
+//        System.out.println("getDescriptor " + xmlTag);
+
+        if (!(xmlTag instanceof HtmlTag && RTProjectComponent.isEnabled(xmlTag.getProject()))) {
+            return null;
+        }
+        final String directiveName = DirectiveUtil.normalizeAttributeName(xmlTag.getName());
+        if (directiveName.equals(RT_REQUIRE)) {
+            return new RTRequireTagDescriptor(RT_REQUIRE, xmlTag);
+        }
+        return null;
+    }
+}
