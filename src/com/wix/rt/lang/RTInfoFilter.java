218424ab8138b91fc355517dc5a516afdcf48b67
ido <idok@wix.com>
2015-05-27 13:55:16 +0300

    fix NPE

--- src/com/wix/rt/lang/RTInfoFilter.java
+++ src/com/wix/rt/lang/RTInfoFilter.java
@@ -3,6 +3,7 @@ package com.wix.rt.lang;
 import com.intellij.codeInsight.daemon.impl.HighlightInfo;
 import com.intellij.codeInsight.daemon.impl.HighlightInfoFilter;
 import com.intellij.psi.PsiFile;
+import org.jetbrains.annotations.NotNull;
 
 /**
  * Created by idok on 4/22/15.
@@ -11,8 +12,8 @@ public class RTInfoFilter implements HighlightInfoFilter {
     private static final String MSG_EMPTY_TAG = "Empty tag doesn't work in some browsers";
 
     @Override
-    public boolean accept(HighlightInfo highlightInfo, PsiFile psiFile) {
-        if (psiFile != null && psiFile.getName().endsWith(".rt") && MSG_EMPTY_TAG.equals(highlightInfo.getDescription())) {
+    public boolean accept(@NotNull HighlightInfo highlightInfo, PsiFile psiFile) {
+        if (psiFile != null && psiFile.getName() != null && psiFile.getName().endsWith(".rt") && MSG_EMPTY_TAG.equals(highlightInfo.getDescription())) {
             return false;
         }
 //        System.out.println(highlightInfo.getSeverity() + " " + highlightInfo.getDescription());
